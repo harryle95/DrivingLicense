f114d742e8c03c13ba0c8dcd7da34184a291d255
hson <lehoangsonsg@gmail.com>
2025-11-28 22:12:15 +1100

    Basic parser and driver

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using src;
+
+public class Program()
+{
+    
+    public static void Main(string[] arguments)
+    {
+        if (arguments.Length == 0)
+            throw new ArgumentException("Question file argument required.");
+        if (arguments.Length != 1)
+            throw new ArgumentException($"The program expects only one argument - path of question file. {arguments.Length} arguments received");
+        var filePath = arguments[0];
+        QuestionParser parser = new(filePath);
+        List<Question> questions = parser.Questions;
+        foreach (var q in questions){
+            Console.WriteLine(q);
+        }
+    }
+}
